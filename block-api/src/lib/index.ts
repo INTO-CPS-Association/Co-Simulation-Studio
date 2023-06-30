98b003c176f348a2655e13033e076799bbd1e4c6
Mohamad Omar Nachawati <omar@perpetuallabs.io>
2023-06-30 20:25:46 +0300

    Initial commit

---
+++ block-api/src/lib/index.ts
@@ -0,0 +1,13 @@
+
+class MyLibrary {
+  constructor() {
+    console.log("Library constructor loaded");
+  }
+
+  myMethod = (): boolean => {
+    console.log("Library method fired");
+    return true;
+  };
+}
+
+export default MyLibrary;
