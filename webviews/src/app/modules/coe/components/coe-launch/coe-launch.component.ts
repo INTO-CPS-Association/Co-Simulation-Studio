18a62adc707deff78bc45267668648c679351fda
emil5654 <emil.hl@hotmail.com>
2023-08-01 14:10:52 +0200

    Added some FIXME's

--- webviews/src/app/modules/coe/components/coe-launch/coe-launch.component.ts
+++ webviews/src/app/modules/coe/components/coe-launch/coe-launch.component.ts
@@ -9,17 +9,22 @@ import { MaestroApiService, maestroVersions } from 'src/app/modules/shared/servi
 })
 export class CoeLaunchComponent {
 
+  //FIXME is non-angular interface 
   _coeIsOnlineSub: Subscription;
   online: boolean = false;
   correctCoeVersion: boolean = true;
+  //FIXME is non-angular interface 
   coeVersions = maestroVersions;
 
+  //FIXME is non-angular interface 
   @Input()
   required_coe_version!: maestroVersions;
 
+  //FIXME is non-angular interface 
   @Input()
   coeLaunchClick!: Subject<void>;
 
+  //FIXME is non-angular interface 
   constructor(public coeSimulationService: MaestroApiService) {
     this._coeIsOnlineSub = coeSimulationService.startMonitoringOnlineStatus(isOnline => {
       if (this.required_coe_version) {
@@ -29,10 +34,11 @@ export class CoeLaunchComponent {
     });
   }
 
+  //FIXME is non-angular interface 
   ngOnDestroy(): void {
     this.coeSimulationService.stopMonitoringOnlineStatus(this._coeIsOnlineSub);
   }
-
+//FIXME is non-angular interface 
   onCoeLaunchClick() {
     if (this.coeLaunchClick) {
       this.coeLaunchClick.next();
