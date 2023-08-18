d3c8ffc57dcf2669011c1a135576f0bc3a5a9bf2
emil5654 <emil.hl@hotmail.com>
2023-08-18 11:55:17 +0200

    migrate sampling rate

--- components/src/coe/sampling-rate/sampling-rate.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class SamplingRate extends Component {
-  render() {
-    return <div>SamplingRate</div>;
-  }
-}
-
-export default SamplingRate;
---
+++ components/src/coe/sampling-rate/sampling-rate.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type SamplingRateComponent_props = {
+  constraint: any
+  editing: boolean
+  formGroup: any
+}
+export const SamplingRateComponent: React.FunctionComponent<SamplingRateComponent_props>= ({ constraint, editing, formGroup }) => {
+    return (
+        <div>
+           
+        </div>
+    );
+};
+
+export default SamplingRateComponent;
