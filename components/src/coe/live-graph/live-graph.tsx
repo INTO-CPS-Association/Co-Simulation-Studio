369a01257cb5321101efb383ad045ee1c7a4d1f9
emil5654 <emil.hl@hotmail.com>
2023-08-21 09:59:11 +0200

    updated with html content

--- components/src/coe/live-graph/live-graph.tsx
+++ components/src/coe/live-graph/live-graph.tsx
@@ -65,9 +65,75 @@ export const LiveGraphComponent: React.FunctionComponent<LiveGraphComponent_prop
     };
     
     return (
-        <div>
+      <div className="form-horizontal" //FIXMEformGroup={formGroup}
+      >
+      <div className="form-group">
+        <div className="form-horizontal">
+          <div className="form-group">
+            <label className="col-sm-5 col-md-4 control-label">Name:</label>
+            <p className="form-control-static col-sm-7 col-md-8">{editing ? graph.title : ''}</p>
+            {editing && (
+              <input
+                className="form-control col-sm-7 col-md-8"
+                name="title"
+                value={graph.title}
+                //onChange={(e) => this.onTitleChange(e.target.value)}
+              />
+            )}
+          </div>
+          <div className="form-group">
+            <label className="col-sm-5 col-md-4 control-label">External Window</label>
+            <div className="checkbox col-sm-7 col-md-8">
+              <label>
+                <input
+                  name="externalWindow"
+                  type="checkbox"
+                  disabled={!editing}
+                  checked={graph.externalWindow}
+                  //onChange={(e) => this.onExternalWindowChange(e.target.checked)}
+                />
+              </label>
+            </div>
+          </div>
+          
+          {config.multiModel.fmuInstances.map((instance: any, index: number) => ( //FIXME fmuinstances not defined
+            <div className="form-group" key={index}>
+              <label className="col-sm-5 col-md-4 control-label">
+                {instance.fmu.name}.{instance.name}
+              </label>
+              <div className="col-sm-7 col-md-8">
+                {editing
+                  ? getOutputs(instance.fmu.scalarVariables).map((output: any, outIndex: number) => (
+                      <div className="checkbox" key={outIndex}>
+                        <label>
+                          <span
+                            className={isLocal(output) ? 'text-warning' : ''}
+                            title={`Name: ${output.name}, Causality: ${output.causality}, Type: ${getScalarVariableTypeName(output.type)}`}
+                          >
+                            {output.name}
+                          </span>
+                        </label>
+                      </div>
+                    ))
+                  : restrictToCheckedLiveStream(instance, getOutputs(instance.fmu.scalarVariables)).map((output: any, outIndex: number) => (
+                      <div className="checkbox" key={outIndex}>
+                        <label>
+                          <span
+                            className={isLocal(output) ? 'text-warning' : ''}
+                            title={`Name: ${output.name}, Causality: ${output.causality}, Type: ${getScalarVariableTypeName(output.type)}`}
+                          >
+                            {output.name}
+                          </span>
+                        </label>
+                      </div>
+                    ))}
+              </div>
+            </div>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default LiveGraphComponent;
