95e1f8bed760b997912d88e75fd355569afc54d1
emilhu20 <emil.hu@hotmail.com>
2023-10-09 23:25:29 +0200

    Revert "remove code from extension to hide some components from tree view"
    This reverts commit 2f5e6106251ac57a9b7ab622105e74b0b9bb2698.

--- extension/src/browser/main.ts
+++ extension/src/browser/main.ts
@@ -1,14 +1,18 @@
 import * as vscode from 'vscode';
 import { CoeEditorProvider } from '../common/coeEditorProvider';
+import { DseEditorProvider } from '../common/dseEditorProvider';
+import { DseTreeDataProvider } from '../common/dseTreeDataProvider';
 import { FmuTreeDataProvider } from '../common/fmuTreeDataProvider';
 import { MmEditorProvider } from '../common/mmEditorProvider';
 import { ModelTreeDataProvider } from '../common/modelTreeDataProvider';
 import { MultiModelTreeDataProvider } from '../common/multiModelTreeDataProvider';
+import { SysMLTreeDataProvider } from '../common/sysmlTreeDataProvider';
 
 export function activate(context: vscode.ExtensionContext) {
 	vscode.workspace.getConfiguration().update('workbench.iconTheme', 'vscode-icons');
 
 	context.subscriptions.push(CoeEditorProvider.register(context));
+	context.subscriptions.push(DseEditorProvider.register(context));
 	context.subscriptions.push(MmEditorProvider.register(context));
 
 	context.subscriptions.push(vscode.window.createTreeView('cosim-fmus', { treeDataProvider: new FmuTreeDataProvider(context) }));
--- extension/src/node/main.ts
+++ extension/src/node/main.ts
@@ -2,16 +2,20 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import { workspace, ExtensionContext } from 'vscode';
 
+import { DseTreeDataProvider } from '../common/dseTreeDataProvider';
 import { FmuTreeDataProvider } from '../common/fmuTreeDataProvider';
 import { ModelTreeDataProvider } from '../common/modelTreeDataProvider';
 import { MultiModelTreeDataProvider } from '../common/multiModelTreeDataProvider';
+import { SysMLTreeDataProvider } from '../common/sysmlTreeDataProvider';
 import { CoeEditorProvider } from '../common/coeEditorProvider';
+import { DseEditorProvider } from '../common/dseEditorProvider';
 import { MmEditorProvider } from '../common/mmEditorProvider';
 
 export function activate(context: ExtensionContext) {
 	vscode.workspace.getConfiguration().update('workbench.iconTheme', 'vscode-icons');
 
 	context.subscriptions.push(CoeEditorProvider.register(context));
+	context.subscriptions.push(DseEditorProvider.register(context));
 	context.subscriptions.push(MmEditorProvider.register(context));
 
 	context.subscriptions.push(vscode.window.createTreeView('cosim-fmus', { treeDataProvider: new FmuTreeDataProvider(context) }));
